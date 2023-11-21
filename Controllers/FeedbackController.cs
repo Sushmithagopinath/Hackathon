e4ae1ae2d459a76360ffdfaef7a7d38f1992dc5e
Sushmithagopinath <45627060+Sushmithagopinath@users.noreply.github.com>
2023-11-21 12:52:24 +0530

    Add files via upload
    test

---
+++ Controllers/FeedbackController.cs
@@ -0,0 +1,51 @@
+﻿namespace FeedbackSentimentAnalysis.Controllers
+{
+    using FeedbackSentimentAnalysis.App.Mgr.FeedbackManager;
+    using FeedbackSentimentAnalysis.App.Mgr.IncidentManager;
+    using FeedbackSentimentAnalysis.Contracts;
+    using Microsoft.AspNetCore.Mvc;
+
+
+    [Route("api/feedback/")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly IFeedbackManager feedbackManager;
+        private readonly IIncidentManager incidentManager;
+
+
+        public FeedbackController(IFeedbackManager feedbackManager, IIncidentManager incidentManager)
+        {
+            this.feedbackManager = feedbackManager;
+            this.incidentManager = incidentManager;
+        }
+
+        [Route("sentiment/ai-response")]
+        [HttpPost]
+        public async Task<IList<Feedbacksentiment>> GetFeedbackSetiment([FromBody] List<string> feedbackList, bool includeOpenionMining)
+        {
+            return await feedbackManager.GetSentimentResultsAsync(feedbackList, includeOpenionMining);
+        }
+
+        [Route("sentiment/ai-response/final")]
+        [HttpPost]
+        public async Task<string> GetAggregateSentiment([FromBody] List<string> feedbackList, bool includeOpenionMining)
+        {
+            return await feedbackManager.GetAggregateSentiment(feedbackList, includeOpenionMining);
+        }
+
+        [Route("sentiment/gen-ai-response")]
+        [HttpPost]
+        public async Task<SentimentResponse> GetFeedbackSentimentFromGenAI(string incidentNumber)
+        {
+            IncidentDetails incidentDetails = await incidentManager.GetIncidentComments(incidentNumber);
+            if (incidentDetails is null)
+            {
+                return new SentimentResponse();
+            }
+            var sentimentResponse = await feedbackManager.GetSentimentFromGenAI(incidentDetails.Result.Comments);
+            sentimentResponse.IncidentStatus = incidentDetails.Result.Incident_State;
+            return sentimentResponse;
+        }
+    }
+}
---
+++ Controllers/IncidentController.cs
@@ -0,0 +1,37 @@
+﻿namespace FeedbackSentimentAnalysis.Api.Controllers
+{
+    using FeedbackSentimentAnalysis.App.Mgr.IncidentManager;
+    using FeedbackSentimentAnalysis.Contracts;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+
+    [Route("api/incident")]
+    [ApiController]
+    public class IncidentController : ControllerBase
+    {
+
+        private readonly IIncidentManager incidentManager;
+
+        public IncidentController(IIncidentManager incidentManager)
+        {
+            this.incidentManager = incidentManager;
+        }
+
+        [Route("system/details")]
+        [HttpGet]
+        public async Task<Incident> GetIncidentSystemDetails(string  incidentNumber)
+        {
+           return await incidentManager.GetIncidentSysId(incidentNumber);
+        }
+
+
+        [Route("system/work-notes")]
+        [HttpGet]
+        public async Task<IncidentDetails> GetIncidentDetailsAsync(string incidentNumber)
+        {
+            return await incidentManager.GetIncidentComments(incidentNumber);
+        }
+
+    }
+}
